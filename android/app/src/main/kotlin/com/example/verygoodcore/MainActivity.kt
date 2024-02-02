5d9fed364a3cbe3dbad44ea61084b5182168de77
Alfredo Paez <alfredo123.apne@gmail.com>
2024-02-01 20:45:37 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.catbreeds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:catbreeds/presentation/app/app.dart';
+import 'package:catbreeds/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
